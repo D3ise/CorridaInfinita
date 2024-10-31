2153bca0951492582aa587cc5a066d0b85cb7c81
deise <deiseelen17@gmail.com>
2024-10-31 08:56:42 -0300

    Salvo

--- CorridaInfinita/MainPage.xaml.cs
+++ CorridaInfinita/MainPage.xaml.cs
@@ -2,12 +2,35 @@
 
 public partial class MainPage : ContentPage
 {
+    int velocidade = 10;
+	int larguraJanela=0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	
+	async Task Desenhar()
+	{
+       GerenciaImagens();
+	}
+
+	void GerenciaImagens ()
+	{
+		fundo.TranslationX-= velocidade;
+		fundopredio.TranslationX-= velocidade;
+		fundoparque.TranslationX-= velocidade;
+		chao.TranslationX-= velocidade;
+		animal.TranslationX-= velocidade;
+		if (fundo.TranslationX<-larguraJanela)
+		{
+			fundo.TranslationX=0;
+			fundopredio.TranslationX=0;
+			fundoparque.TranslationX=0;
+			chao.TranslationX=0;
+			animal.TranslationX=0;
+			
+		}
+	}
 }
 
